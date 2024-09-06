0ffa42de49ac770ba08830f4113230b86d330431
maishaparedes <maisha.paredes@ucas-edu.net>
2024-09-06 10:32:35 -0600

    whoAreYou

---
+++ First_project/whoAreYou/main.py
@@ -0,0 +1,6 @@
+#maisha whoAreYou
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+Favoritecolor = input("Enter your favorite color: ")
+
+print(f"Hello, My name is {name} and I'm {age} years old. My favorite color is {Favoritecolor}")
