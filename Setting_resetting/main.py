5178922c81312b20f057c5b905e2f2cd81055e17
maishaparedes <maisha.paredes@ucas-edu.net>
2024-09-12 10:36:44 -0600

    whoAreYou

---
+++ Setting_resetting/main.py
@@ -0,0 +1,17 @@
+staff =32
+students = 100 
+guests=students*2 
+seats =12
+
+
+
+print((staff+students+guests)/seats)
+
+staff =30
+students = 100 -1
+guests=students*2 -15
+seats =12
+
+
+
+print((staff+students+guests)/seats)
--- whoAreYou/main.py
+++ whoAreYou/main.py
@@ -3,4 +3,4 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 Favoritecolor = input("Enter your favorite color: ")
 
-print(f"Hello, My name is {name} and I'm {age} years old. My favorite color is {Favoritecolor}")
+print("Hello, My name is {name} and I'm {age} years old. My favorite color is {Favoritecolor}")
