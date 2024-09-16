a625617ba8c570929e868b0964fc1792faef2a45
maishaparedes <maisha.paredes@ucas-edu.net>
2024-09-16 10:23:03 -0600

    Celsius_toferenheit

--- Celsius_toFarenheit/main.py
+++ Celsius_toFarenheit/main.py
@@ -2,4 +2,4 @@ celsius= input("Enter the temperature in celsius: ")
 
 farenheit= (int(celsius) * 9/5)+32
 
-print("When the temperature is {100} in celsuis, it will be {Fahrenheit}")
+print(f"When the temperature is {celsius} in celsuis, it will be {farenheit}")
