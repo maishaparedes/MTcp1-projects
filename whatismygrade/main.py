ae29ad339f4a06565d066e40212724f72c5df5ea
maishaparedes <maisha.paredes@ucas-edu.net>
2024-11-04 10:43:37 -0700

    skillpractice_whatisgrade

---
+++ whatismygrade/main.py
@@ -0,0 +1,13 @@
+print("Hello user!")
+grade = int(input("What is your grade percentage?"))
+
+if grade >=90: print("Your grade is an A, Great job!")
+
+elif grade >=80 and grade < 90: print("Your grade is a B, nice work!")
+
+elif grade >=70 and grade < 80: print("Your grade is a C, could be better!")
+
+elif grade >=60 and grade <70: print("Your grade is a D, don't keep that up!")
+
+else: 
+   print("You have an F, fix that man!")
