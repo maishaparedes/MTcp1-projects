5f3977a5cb7101ffbcb018ee3bd50631e6f3adf7
maishaparedes <maisha.paredes@ucas-edu.net>
2024-11-06 10:24:52 -0700

    Proficiency_test

---
+++ Authorized/main.py
@@ -0,0 +1,16 @@
+
+user = ["natalia", "maisha", "Trey" , "Brianna", "Pascal" , "Jayce" , "Mariana" , "Paul" , "pedro"]
+
+
+name = input("What is your name?")
+
+
+if name == "maisha":
+    print("Hello admin!")
+
+elif name in user:
+    print("You have access! Woohoo!")
+
+else: 
+    print("You are not allowed to gain entry")
+
