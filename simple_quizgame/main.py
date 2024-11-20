705a7a44ff5116b958b94ea6f338114ebd406318
maishaparedes <maisha.paredes@ucas-edu.net>
2024-11-20 10:51:27 -0700

    quizgame

---
+++ simple_quizgame/main.py
@@ -0,0 +1,84 @@
+#Maisha Tapia Paredes, proficiency test
+
+score = 0 
+global question_1 , question_2 , question_3 , question_4, question_5
+
+question_1 = input("Who is luigis nemisis? \n A. Mario \n B. Waluigi \n C. Egg guy \n D. Sonic ")
+if x.lower == "B":
+    print("Correct")
+    score += 1
+
+else: 
+    score += 0
+print(score)
+print("Nope!")
+
+question_2 = input("Which character in Super Smash Bros Ultamite has the same powers as princess peach? \n A. Donkey Kong \n B. Rosalina \n C. Daisy \n")
+if x.lower == "C":
+    print("Correct")
+    score += 1
+
+else: 
+    score += 0
+print(score)
+print("No..") 
+
+question_3 = input("A game was made where Wario and Waluigi first made appearances in nintendo, the theme of the game was a sport. Which sport?  \n A. Volleyball \n B. Football \n C. Lacrosse \n D. Tennis ")
+if x.lower == "B":
+    print("Correct")
+    score += 1
+
+else: 
+    score += 0
+print(score)
+print("Nope!")
+
+
+
+
+
+
+
+if x == "tennis":
+    score += 1
+
+else: 
+    score += 0
+print(score)
+
+x = input("What games does Rosalina come from?")
+if x == "Mario galaxy":
+    score += 1
+
+else: 
+    score += 0
+print(score)
+
+
+x = input("Which mario brother marries princess peach?")
+if x == "Mario":
+    score += 1
+
+else: 
+    score += 0
+print(score)
+   
+
+x = input("What is the most difficult Super Mario Bros game?")
+if x == "Super mario odyssey":
+    score += 1
+
+else: 
+    score += 0
+print(score)
+  
+
+x = input("Who is luigis nemisis?")
+if x == "Walugi":
+    score += 1
+
+else: 
+    score += 0
+print(score)
+ 
+
