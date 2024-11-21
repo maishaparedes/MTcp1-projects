1041c54aca3208bec68cc4745dceee6baf37c266
maishaparedes <maisha.paredes@ucas-edu.net>
2024-11-21 10:35:40 -0700

    simplequiz

--- simple_quizgame/main.py
+++ simple_quizgame/main.py
@@ -3,82 +3,59 @@
 score = 0 
 global question_1 , question_2 , question_3 , question_4, question_5
 
+
 question_1 = input("Who is luigis nemisis? \n A. Mario \n B. Waluigi \n C. Egg guy \n D. Sonic ")
-if x.lower == "B":
+if question_1.lower() == "b":
     print("Correct")
     score += 1
 
-else: 
+elif question_1.lower() != "b":
+    print("Nope!")
     score += 0
 print(score)
-print("Nope!")
 
-question_2 = input("Which character in Super Smash Bros Ultamite has the same powers as princess peach? \n A. Donkey Kong \n B. Rosalina \n C. Daisy \n")
-if x.lower == "C":
+question_2 = input("Which character in Super Smash Bros Ultamite has the same powers as princess peach? \n A. Donkey Kong \n B. Rosalina \n C. Daisy \n D. Toadette ")
+if question_2.lower() == "c":
     print("Correct")
     score += 1
 
-else: 
+elif question_2.lower() != "c":
+    print("Nope!")
     score += 0
 print(score)
-print("No..") 
 
 question_3 = input("A game was made where Wario and Waluigi first made appearances in nintendo, the theme of the game was a sport. Which sport?  \n A. Volleyball \n B. Football \n C. Lacrosse \n D. Tennis ")
-if x.lower == "B":
+if question_3.lower() == "d":
     print("Correct")
     score += 1
 
-else: 
+elif question_3.lower() != "d":
+    print("wrong.")
     score += 0
 print(score)
-print("Nope!")
 
-
-
-
-
-
-
-if x == "tennis":
+question_4 = input("What game does the nintendo character rosalina come from?  \n A. Super mario RPG \n B. Mario Party Superstars \n C. Mario galaxy  \n D. Mario kart 8 deluxe ")
+if question_4.lower() == "c":
+    print("Correct")
     score += 1
 
-else: 
+elif question_4.lower() != "c":
+    print("No, thats not right!")
     score += 0
-print(score)
 
-x = input("What games does Rosalina come from?")
-if x == "Mario galaxy":
+question_5 = input("""Which Mario brother marries princess peach? 
+                   A. Luigi 
+                   B. Wario
+                   C. Mario
+                   D. Mario 2.0 """)
+if question_5.lower() == "d":
+    print("Yep!")
     score += 1
 
-else: 
+elif question_5.lower() != "d":
+    print("No, thats not right!")
     score += 0
-print(score)
 
 
-x = input("Which mario brother marries princess peach?")
-if x == "Mario":
-    score += 1
 
-else: 
-    score += 0
-print(score)
-   
-
-x = input("What is the most difficult Super Mario Bros game?")
-if x == "Super mario odyssey":
-    score += 1
-
-else: 
-    score += 0
-print(score)
-  
-
-x = input("Who is luigis nemisis?")
-if x == "Walugi":
-    score += 1
-
-else: 
-    score += 0
-print(score)
- 
 
