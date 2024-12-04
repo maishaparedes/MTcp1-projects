c07c3774a77babb5aa3c7983b3b3813bef752141
maishaparedes <maisha.paredes@ucas-edu.net>
2024-12-04 10:19:57 -0700

    rock_python_paper

--- rock_paper_scissors/rock.py
+++ rock_paper_scissors/rock.py
@@ -3,18 +3,20 @@ score = 0
 
 com1 = ["rock", "paper" , "scissors"]
 inside = random.choice(com1)
-user = input("Choose rock paper or scissors!")
 
-while user not in ["rock, paper, scissors"]:
-    print("Try again")
-    user = input("Rock paper scissors")
-    if user in ["rock, paper, scissors"]: 
-        break
-    
 ask = input("Would you like to play?:\n ")
 if ask == "no":
     print("Okay, you don't gotta play, bye!")
+    
 while ask == "yes":
+    user = input("Choose rock paper or scissors!")
+
+    while not user in ["rock", "paper", "scissors"]:
+        print("Try again")
+        user = input("Choose rock paper or scissors!")
+
+
+
     if user == "scissors" and inside == "rock":
         print("You lost this part")
         score -= 1
@@ -45,6 +47,7 @@ while ask == "yes":
         print(score)
     else:
         print("it is a tie. No points")
+
 
 
 
